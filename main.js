2764876d001352d2326043d6fba1d2f6d0b60285
Umor Faruk <101555985+UmorFaruk69@users.noreply.github.com>
2022-12-11 19:56:02 +0600

    java

---
+++ main.js
@@ -0,0 +1,9 @@
+var z = prompt("Enter Letter : ");
+
+x = z.toLowerCase();
+
+if (x == "a" || x == "e" || x == "i" || x == "o" || x == 'u')
+   document.write("Vowel");
+   
+else
+   document.write("Consonant");
